65771566d2a662c0f6c5564b606c3bafbb8e9f1b
DESKTOP-58UIFAP\user <anjin0910@gmail.com>
2026-04-28 10:55:20 +0900

    Fix multiple main classes issue by deleting DemoApplication and updating component scan

--- src/main/java/com/example/integrated/IntegratedPracticeApplication.java
+++ src/main/java/com/example/integrated/IntegratedPracticeApplication.java
@@ -3,7 +3,7 @@ package com.example.integrated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
 public class IntegratedPracticeApplication {
     public static void main(String[] args) {
         SpringApplication.run(IntegratedPracticeApplication.class, args);
